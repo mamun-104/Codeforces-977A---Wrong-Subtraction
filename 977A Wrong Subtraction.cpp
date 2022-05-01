5cadd1ca5f9cb6aae3beaf968155f07c7a07c250
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2022-05-01 21:23:38 +0600

    Add files via upload

---
+++ 977A Wrong Subtraction.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    int temp,i;
+    cin>>n>>k;
+
+    for(i=0;i<k;i++)
+    {
+        if(n%10==0){n=n/10;}
+        else
+        {
+            n=n-1;
+        }
+    }
+    cout<<n<<endl;
+
+
+    return 0;
+}
+
+
+
+
+
